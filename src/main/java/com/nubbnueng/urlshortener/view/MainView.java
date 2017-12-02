0d15016d9c843a181a5de18966ea75bc665b8367
Panithan Sophontammakhun <nubb.1@hotmail.com>
2017-12-02 23:41:00 +0700

    add button click listener to generate short url link

--- src/main/java/com/nubbnueng/urlshortener/view/MainView.java
+++ src/main/java/com/nubbnueng/urlshortener/view/MainView.java
@@ -1,5 +1,8 @@
 package com.nubbnueng.urlshortener.view;
 
+import java.nio.charset.StandardCharsets;
+
+import com.google.common.hash.Hashing;
 import com.vaadin.annotations.Theme;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.navigator.View;
@@ -19,16 +22,18 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 public class MainView extends VerticalLayout implements View {
-
+	
+	final static String hostUrl = "http://localhost:8080/";
+	
 	Label headerLabel;
 	TextField urlTextField;
 	Button shortenUrlButton;
 	Link shortUrlLink;
-	
-    @Override
-    public void enter(ViewChangeEvent event) {
-    }
-	
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+	}
+
 	public MainView() {
 		setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
 		addHeader();
@@ -46,20 +51,26 @@ public class MainView extends VerticalLayout implements View {
 		urlTextField = new TextField();
 		urlTextField.focus();
 		urlTextField.setWidth("100%");
-		
+
 		shortenUrlButton = new Button("SHORTEN !");
 		shortenUrlButton.setStyleName(ValoTheme.BUTTON_PRIMARY);
 		shortenUrlButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
-		
+		shortenUrlButton.addClickListener(click -> {
+			String originalUrl = urlTextField.getValue();
+			String shortUrl = hostUrl + Hashing.murmur3_32().hashString(originalUrl, StandardCharsets.UTF_8).toString();
+			
+			shortUrlLink.setCaption(shortUrl);
+			shortUrlLink.setResource(new ExternalResource(shortUrl));
+		});
+
 		HorizontalLayout formLayout = new HorizontalLayout();
 		formLayout.addComponents(urlTextField, shortenUrlButton);
 		formLayout.setExpandRatio(urlTextField, 1);
 		formLayout.setWidth("60%");
 		addComponent(formLayout);
 	}
-	
-	private void addResult() {
-		final String hostUrl = "http://localhost:8080/";
+
+	private void addResult() {		
 		shortUrlLink = new Link(hostUrl, new ExternalResource(hostUrl));
 
 		addComponent(shortUrlLink);
