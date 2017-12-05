07ccf7ea502d3d07ba92f33857d00f250128d928
Panithan Sophontammakhun <nubb.1@hotmail.com>
2017-12-05 13:20:19 +0700

    add login/logout state to nav button

--- src/main/java/com/nubbnueng/urlshortener/service/UserService.java
+++ src/main/java/com/nubbnueng/urlshortener/service/UserService.java
@@ -6,13 +6,23 @@ import org.springframework.stereotype.Service;
 public class UserService {
 	private final String username = "admin";
 	private final String password = "p@ssw0rd";
+	private static boolean login = false;
 	
 	public UserService() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public boolean doAuthenticate(String username, String password) {
-		return this.username.equals(username) && this.password.equals(password);
+		login = this.username.equals(username) && this.password.equals(password);
+		return login;
+	}
+	
+	public boolean isLogin() {
+		return login;
+	}
+	
+	public void logout() {
+		UserService.login = false;
 	}
 	
 }
--- src/main/java/com/nubbnueng/urlshortener/view/TemplateView.java
+++ src/main/java/com/nubbnueng/urlshortener/view/TemplateView.java
@@ -1,54 +1,84 @@
 package com.nubbnueng.urlshortener.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nubbnueng.urlshortener.service.UserService;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
-public class TemplateView extends VerticalLayout implements View{
+public class TemplateView extends VerticalLayout implements View {
+
+	protected UserService userService;
+	protected VerticalLayout contentLayout;
+	protected Navigator navigator;
 	
-	private VerticalLayout contentLayout;
-	private Navigator navigator;
-	
-	public TemplateView(VerticalLayout contentLayout, Navigator navigator) {
+	protected Button loginNav;
+	protected Button logoutNav;
+
+	public TemplateView(VerticalLayout contentLayout, Navigator navigator, UserService userService) {
 		this.contentLayout = contentLayout;
 		this.navigator = navigator;
+		this.userService = userService;
+
 		setSpacing(true);
 		setWidth("100%");
 		setMargin(false);
 		setDefaultComponentAlignment(Alignment.TOP_CENTER);
-		
+
 		addNavigatorBar();
 		addContent();
 	}
 	
+    @Override
+    public void enter(ViewChangeEvent event) {
+        refreshNavbar();
+    }
+
+	private void refreshNavbar() {
+		loginNav.setVisible(!userService.isLogin());
+		logoutNav.setVisible(userService.isLogin());
+	}
+
 	private void addNavigatorBar() {
 		HorizontalLayout navbarLayout = new HorizontalLayout();
 		navbarLayout.setStyleName(ValoTheme.LAYOUT_CARD);
 		navbarLayout.setWidth("100%");
-		
+
 		HorizontalLayout navbar = new HorizontalLayout();
 		navbar.setMargin(false);
 		navbar.setSpacing(false);
-		
+
 		Button homeNav = new Button("Home");
 		homeNav.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
 		homeNav.addClickListener(click -> navigator.navigateTo(""));
-		
-		Button loginNav = new Button("Login");
+
+		loginNav = new Button("Login");
 		loginNav.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
+		loginNav.setVisible(!userService.isLogin());
 		loginNav.addClickListener(click -> navigator.navigateTo("login"));
-		
-		navbar.addComponents(homeNav,loginNav);
+
+		logoutNav = new Button("Logout");
+		logoutNav.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
+		logoutNav.setVisible(userService.isLogin());
+		logoutNav.addClickListener(click -> {
+			userService.logout();
+			navigator.navigateTo("");
+		});
+
+		navbar.addComponents(homeNav, loginNav, logoutNav);
 		navbarLayout.addComponent(navbar);
-		
+
 		addComponent(navbarLayout);
 	}
-	
+
 	private void addContent() {
 		addComponent(contentLayout);
 	}
