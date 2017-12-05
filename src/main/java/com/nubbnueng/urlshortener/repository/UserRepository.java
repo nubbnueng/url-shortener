caeb1e46fc27c54226163bd5f28510e93b5c05af
Panithan Sophontammakhun <nubb.1@hotmail.com>
2017-12-05 14:32:38 +0700

    add user repository

---
+++ src/main/java/com/nubbnueng/urlshortener/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nubbnueng.urlshortener.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nubbnueng.urlshortener.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	public User findByUsername(String username);
+}
