f1131bb663153cca26c9f8da85013103a7ec7471
Panithan Sophontammakhun <nubb.1@hotmail.com>
2017-12-05 15:39:07 +0700

    implement authenticate by look up in database

--- src/main/java/com/nubbnueng/urlshortener/service/UserService.java
+++ src/main/java/com/nubbnueng/urlshortener/service/UserService.java
@@ -1,19 +1,24 @@
 package com.nubbnueng.urlshortener.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.nubbnueng.urlshortener.model.User;
+import com.nubbnueng.urlshortener.repository.UserRepository;
 
 @Service
 public class UserService {
-	private final String username = "admin";
-	private final String password = "p@ssw0rd";
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	private static boolean login = false;
 	
-	public UserService() {
-		// TODO Auto-generated constructor stub
-	}
-	
 	public boolean doAuthenticate(String username, String password) {
-		login = this.username.equals(username) && this.password.equals(password);
+		User user = userRepository.findByUsername(username);
+		if (user != null) {
+			login = password.equals(user.getPassword());
+		}
 		return login;
 	}
 	
